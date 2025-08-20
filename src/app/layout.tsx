764a0377c514472f146be79477ffd9b5b232097c
Vinciarya <kosliaaryan@gmail.com>
2025-08-20 09:57:17 +0530

    Added new components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Bowlby_One_SC, DM_Mono } from "next/font/google";
 import { Header } from "@/components/Header";
 import "./globals.css";
+import { Filters } from "@/components/Filters";
 
 const bowlby = Bowlby_One_SC({
   subsets: ["latin"],
@@ -36,6 +37,7 @@ export default function RootLayout({
          <Header/>
         {children}
         </main>
+        <Filters/>
       </body>
     </html>
   );
